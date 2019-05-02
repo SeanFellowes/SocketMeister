c9832a9eccc28e3473e736d11d45dfffc5f4c497
Sean Fellowes <sfellowes@hotmail.com>
2019-05-02 16:00:50 +0200

    Test Harness

--- src/Shared.SocketMeister.Test.Client/TestClientHarness.cs
+++ src/Shared.SocketMeister.Test.Client/TestClientHarness.cs
@@ -1,24 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SocketMeister.Testing;
 
-namespace SocketMeister.Test
+namespace SocketMeister
 {
     public class TestClientHarness
     {
         private readonly SocketClient controlSocket = null;
 
+        /// <summary>
+        /// Event raised when a status of a socket connection has changed
+        /// </summary>
+        public event EventHandler<ConnectionStatusChangedEventArgs> ConnectionStatusChanged;
+
 
         public TestClientHarness()
         {
-            List<SocketEndPoint> endPoints = new List<SocketEndPoint>() { new SocketEndPoint("127.0.0.0", 4505) };
+            List<SocketEndPoint> endPoints = new List<SocketEndPoint>() { new SocketEndPoint("127.0.0.1", 4505) };
             controlSocket = new SocketClient(endPoints);
             controlSocket.ConnectionStatusChanged += controlSocket_ConnectionStatusChanged;
         }
 
+
+        /// <summary>
+        /// The connection status of the socket client
+        /// </summary>
+        public ConnectionStatus ConnectionStatus
+        {
+            get { return (ConnectionStatus)controlSocket.ConnectionStatus; } 
+        }
+
+
         private void controlSocket_ConnectionStatusChanged(object sender, SocketClient.ConnectionStatusChangedEventArgs e)
         {
-            throw new NotImplementedException();
+            ConnectionStatusChanged?.Invoke(this, new ConnectionStatusChangedEventArgs((ConnectionStatus)e.Status, e.IPAddress, e.Port));
+        }
+
+        public void Stop()
+        {
+            controlSocket.Stop();
         }
     }
 }
--- src/Test.Client.Silverlight/MainPage.xaml.cs
+++ src/Test.Client.Silverlight/MainPage.xaml.cs
@@ -14,11 +14,49 @@ namespace SocketMeister.Test.Client.Silverlight
 {
     public partial class MainPage : UserControl
     {
-        TestClientHarness _testClient = null;
+        private TestClientHarness testClient = null;
 
         public MainPage()
         {
             InitializeComponent();
+            StatusColour.Background = new SolidColorBrush(Color.FromArgb(255, 234, 144, 15));
+            StatusText.Text = "Connecting to test server...";
+            testClient = new TestClientHarness();
+            testClient.ConnectionStatusChanged += TestClient_ConnectionStatusChanged;
+
+        }
+
+        private void TestClient_ConnectionStatusChanged(object sender, Testing.ConnectionStatusChangedEventArgs e)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                StatusText.Text = e.IPAddress + ":" + e.Port;
+                if (e.Status == Testing.ConnectionStatus.Connected)
+                {
+                    StatusColour.Background = new SolidColorBrush(Color.FromArgb(255, 0, 150, 0));
+                    StatusText.Text = "Connected to " + e.IPAddress + ":" + e.Port;
+                }
+                else if (e.Status == Testing.ConnectionStatus.Connecting)
+                {
+                    StatusColour.Background = new SolidColorBrush(Color.FromArgb(255, 234, 144, 15));
+                    StatusText.Text = "Connecting to " + e.IPAddress + ":" + e.Port;
+                }
+                else if (e.Status == Testing.ConnectionStatus.Disconnecting)
+                {
+                    StatusColour.Background = new SolidColorBrush(Color.FromArgb(255, 250, 34, 34));
+                    StatusText.Text = "Disconnecting from " + e.IPAddress + ":" + e.Port;
+                }
+                else
+                {
+                    StatusColour.Background = new SolidColorBrush(Color.FromArgb(255, 131, 131, 131));
+                    StatusText.Text = "Disconnected from " + e.IPAddress + ":" + e.Port;
+                }
+            });
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            testClient.Stop();
         }
     }
 }
