44f05268951ae09b2229e6d6d4041496a5e1b937
Sean Fellowes <sfellowes@hotmail.com>
2020-10-02 08:53:38 +1000

    Nuget Package 2.0.4

--- src/SocketMeister (Silverlight)/Properties/AssemblyInfo.cs
+++ src/SocketMeister (Silverlight)/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SocketMeister for Silverlight")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SocketMeister for Silverlight")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SocketMeister")]
-[assembly: AssemblyCopyright("Copyright © Sean Fellowes 2019")]
+[assembly: AssemblyCopyright("Copyright © Sean Fellowes 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("2.0.4")]
+[assembly: AssemblyFileVersion("2.0.4")]
