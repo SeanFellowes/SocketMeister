d38844b612093bc507d566dcf3ade44f2dadc6ce
Sean Fellowes <sfellowes@hotmail.com>
2024-12-29 09:09:34 +1100

    Version 4.1.1

--- src/Code/GlobalAssemblyInfo.cs
+++ src/Code/GlobalAssemblyInfo.cs
@@ -23,7 +23,7 @@
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.1.0.0")]
-[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: AssemblyVersion("4.1.1.0")]
+[assembly: AssemblyFileVersion("4.1.1.0")]
 
 
