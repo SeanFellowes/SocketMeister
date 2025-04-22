54c7692a7bce7cc979320ae0da5d3ccdec8330b5
Sean Fellowes <sfellowes@hotmail.com>
2025-04-22 15:07:25 +1000

    Significant project restructuring

--- src/SocketMeister (.NET 3.5)/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("SocketMeister (.NET 3.5)")]
-[assembly: AssemblyDescription("SocketMeister for .NET 3.5")]
-
--- src/SocketMeister (.NET 4.7.2)/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("SocketMeister (.NET 4.7.2)")]
-[assembly: AssemblyDescription("SocketMeister for .NET 4.7.2")]
